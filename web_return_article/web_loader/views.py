595c6b05bfa261b6cbf5831276836015604e9f1f
staks <stas.sor@gmail.com>
2023-06-05 08:37:10 +0300

    добавил исключение

--- web_return_article/web_loader/views.py
+++ web_return_article/web_loader/views.py
@@ -4,27 +4,36 @@ from django.shortcuts import render
 
 
 def home(request):
+    global message_station
     if request.method == 'POST':
         url = request.POST['url']
         pr_url(url)
-
-        # Validate URL format using regex
-        regex = re.compile(
-            r'^(?:http|ftp)s?://'  # http:// or https:// or ftp:// or ftps://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-            r'localhost|'  # localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or IP
-            r'(?::\d+)?'  # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE
-        )
-        if not re.match(regex, url):
-            message = 'Данный адрес не соответствует.'
+        if url == '':
+            message = 'Введите URL.'
+            return render(request, 'home.html', {'message': message})
         else:
-            # Do something with the URL, e.g. save it or process it
+            # Validate URL format using regex
+            regex = re.compile(
+                r'^(?:http|ftp)s?://'  # http:// or https:// or ftp:// or ftps://
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+                r'localhost|'  # localhost...
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or IP
+                r'(?::\d+)?'  # optional port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE
+            )
 
-            # Show thank you message
-            message = 'Все отлично, мне нужно пару минут что бы создать статью.'
-        return render(request, 'home.html', {'message': message})
+            if not re.match(regex, url):
+                message = 'Данный адрес не соответствует.'
+            else:
+
+                # Show thank you message
+                message = 'Все отлично, мне нужно пару минут что бы создать статью.'
+                # if message == 'Все отлично, мне нужно пару минут что бы создать статью.':
+                message_station = "Тут будет статья!!! "
+                # return render(request, "home.html", {"my_variable": message_station})
+
+            return render(request, 'home.html', {'message': message, 'message_station': message_station})
+
     else:
         return render(request, 'home.html')
 
@@ -35,4 +44,5 @@ def pr_url(url_print):
 
     # запустить скрипт script_to_run.py в консоли
 
-    subprocess.run(["python3", "/home/staks/PycharmProjects/ytmp3-dl/ytmp3-dl.py", "-l", "2", f"{url_print}"], check=True)
+    subprocess.run(["python3", "/home/staks/PycharmProjects/ytmp3-dl/ytmp3-dl.py", "-l", "2", f"{url_print}"],
+                   check=True)
